0814b60bf84fb7d6beb06402af60c72e9e91db49
na-toyoshima <na.toyoshim@gmail.com>
2020-03-30 23:40:27 +0900

    second commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'users#top'
+  resources :books, only:[:new, :create, :show, :index, :destroy]
+  resources :users, only:[:show, :index, :edit]
 end
