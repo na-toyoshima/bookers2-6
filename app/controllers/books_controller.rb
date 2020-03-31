d3d0cbd9e17d13718ae56c5f12a17fd60af8087e
na-toyoshima <na.toyoshim@gmail.com>
2020-03-31 20:26:49 +0900

    third commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
+    @user = User.find(params[:id])
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -19,8 +22,12 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
+    book.user_id = current_user.id
+    if book.save
     redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
+    @book = Book.new
   end
 
   def index
   	@users = User.all
+    @book = Book.new
   end
 
   def edit
+    @book = Book.new
   end
 
   def update
