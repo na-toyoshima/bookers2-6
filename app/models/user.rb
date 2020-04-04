dfdab54361809db00ad811c374d12e318cb7d981
na-toyoshima <na.toyoshim@gmail.com>
2020-04-04 21:15:08 +0900

    8th commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          validates :name, presence: true, length: { minimum: 2, maximum: 20 }
          validates :introduction, length: { maximum: 50 }
          has_many :books, dependent: :destroy
+         attachment :profile_image
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3eaac20b5320546228b8963963443244d34ee787b0fb5932861a8b2a07f9bea5c2f64da0b543f4bb805760c3bb7b191079c3e1977bf88e0896183d00d28dd739'
