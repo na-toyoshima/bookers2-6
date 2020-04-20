23d8ae1f1bde70bb57cf6963a801a264db5c27dc
na-toyoshima <na.toyoshim@gmail.com>
2020-04-20 16:05:27 +0900

    finish practical exercise 3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books, only:[:new, :create, :show, :index, :destroy, :edit, :update]
+  resources :books, only:[:new, :create, :show, :index, :destroy, :edit, :update] do
+    resource :favorites, only:[:create, :destroy]
+  end
 
   delete 'books/:book_id/book_comments/:id', to: 'book_comments#destroy', as: 'book_book_comments'
   post 'books/:book_id/book_comments/:id', to: 'book_comments#create'
